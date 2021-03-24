1b105f8f8ff02013decdc93be038f028cc64a1fb
mahmoudegh <48510315+mahmoudegh@users.noreply.github.com>
2021-03-24 16:09:00 +0200

    HTML5

---
+++ #07-Supporting Html5/js/script.js
@@ -0,0 +1,7 @@
+//This Is Used For Old Browsers.
+
+//#We can Include the HTML5 shiv in the <head> of your page in a conditional
+//comment and after any stylesheets to use The HTML5 Shiv script only with 
+//old browsers as IE 8, IE 7, IE 6, etc. And we wont need to creat each element
+//as shown below
+document.creatElement("galal");
